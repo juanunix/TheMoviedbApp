40e61101232e6bb4498c013aadf74de12d651f56
Juan Sanz <juanunix@gmail.com>
2025-04-15 20:48:30 -0400

    Ajust module conf dependencies

---
+++ data/src/main/java/com/juansanz/data/MyClass.kt
@@ -0,0 +1,3 @@
+package com.juansanz.data
+
+class MyClass
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
@@ -14,4 +14,9 @@ dependencyResolutionManagement {
 }
 rootProject.name = "TheMovieDB"
 include(":app")
-include(":buildScr")
+include(":data")
+include(":domain")
+include(":usecases")
+
+
+
