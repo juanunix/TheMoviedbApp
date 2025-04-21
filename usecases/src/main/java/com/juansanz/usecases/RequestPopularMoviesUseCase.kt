df7d6a4bf895cc4c384cb57cb2a71b0ccf88b23f
Juan Sanz <juanunix@gmail.com>
2025-04-21 19:51:21 -0400

    implementation top 20 popular movies

--- usecases/src/main/java/com/juansanz/usecases/RequestPopularMoviesUseCase.kt
+++ usecases/src/main/java/com/juansanz/usecases/RequestPopularMoviesUseCase.kt
@@ -3,10 +3,37 @@ package com.juansanz.usecases
 import com.juansanz.data.MoviesRepository
 import com.juansanz.domain.Movie
 
-const val THRESHOLD = 1000.0
+/**
+ * Caso de uso para obter os filmes populares e selecionar os 20 melhores
+ * com base nos critérios de popularidade, número de votos e média de votos.
+ *
+ * @param moviesRepository Repositório de filmes responsável por recuperar os dados.
+ */
+const val TOP20 = 20
 
 class RequestPopularMoviesUseCase(
     private val moviesRepository: MoviesRepository,
 ) {
-    suspend operator fun invoke(): List<Movie> = moviesRepository.getAllPopularMovies().filter { it.popularity > THRESHOLD }
+    /**
+     * Recupera todos os filmes populares e retorna uma lista contendo os
+     * 20 melhores filmes, ordenados pela popularidade, número de votos e média de votos.
+     *
+     * @return Lista dos 20 filmes mais relevantes.
+     */
+    suspend operator fun invoke(): List<Movie> = moviesRepository.getAllPopularMovies().getTop20Items()
+
+    /**
+     * Extensão para a lista de filmes que ordena os elementos pelos critérios
+     * de popularidade, número de votos e média de votos, retornando os 20 melhores filmes.
+     *
+     * @receiver List<Movie> Lista de filmes a ser ordenada.
+     * @return Lista contendo os 20 filmes mais relevantes.
+     */
+    fun List<Movie>.getTop20Items(): List<Movie> =
+        this
+            .sortedWith(
+                compareByDescending<Movie> { it.popularity }
+                    .thenByDescending { it.voteCount }
+                    .thenByDescending { it.voteAverage },
+            ).take(TOP20)
 }
